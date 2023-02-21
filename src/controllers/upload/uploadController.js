0399cc8329fe6e5e2234ba35dd209e6085c7d327
nurullah7733 <nurullah7733@gmail.com>
2023-02-22 00:57:28 +0600

    changes

--- src/controllers/upload/uploadController.js
+++ src/controllers/upload/uploadController.js
@@ -6,22 +6,21 @@ const {
 
 exports.uploadImages = async (req, res) => {
   try {
+    let cloudinaryUploadedImgUrl;
     const urls = [];
     const files = req.files;
     for (const file of files) {
       const { path } = file;
       urls.push(path);
     }
+    console.log(urls);
+    cloudinaryUploadedImgUrl = await uploadMultipleImages(urls);
 
-    // let cloudinaryUploadedImgUrl = await uploadMultipleImages(urls);
+    urls.map((item) => {
+      fs.unlinkSync(item);
+    });
 
-    // urls.map((item) => {
-    //   fs.unlinkSync(item);
-    // });
-
-    res
-      .status(200)
-      .json({ status: "success", data: "cloudinaryUploadedImgUrl" });
+    res.status(200).json({ status: "success", data: cloudinaryUploadedImgUrl });
   } catch (error) {
     res.status(400).json({ status: "fail", data: error.toString() });
   }
